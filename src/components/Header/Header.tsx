3cc283724bfe188af06f03beb8dcc3b0f465d41b
Khwaja Shaikh <109279460+Shkkhwaja@users.noreply.github.com>
2025-01-08 14:40:15 +0530

    navbar done

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -13,14 +13,12 @@ import { DownOutlined, PlusCircleOutlined } from "@ant-design/icons";
 import Image from "next/image";
 import "../../app/globals.css";
 import img from "../../../public/Images/man-avatar.webp";
-import { IoIosAddCircleOutline } from "react-icons/io";
 
 const Header: React.FC = () => {
   const [searchText, setSearchText] = useState<string>("");
   const [isAvatarHovered, setIsAvatarHovered] = useState(false);
   const [isButtonHovered, setIsButtonHovered] = useState(false);
   const [isButtonTranslate, setIsButtonTranslate] = useState(false);
-
   const [isDarkMode, setIsDarkMode] = useState(false);
 
   const profileMenuItems = [
@@ -71,9 +69,7 @@ const Header: React.FC = () => {
   useEffect(() => {
     // Check the saved theme preference on component mount
     const savedTheme = localStorage.getItem("hs_theme");
-    const prefersDark = window.matchMedia(
-      "(prefers-color-scheme: dark)"
-    ).matches;
+    const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
 
     if (savedTheme === "dark" || (savedTheme === "auto" && prefersDark)) {
       document.documentElement.classList.add("dark");
@@ -125,13 +121,11 @@ const Header: React.FC = () => {
           </ul>
 
           {/* Search Bar */}
-          <div className="absolute left-[36em] top-7 transform z-0 -translate-y-1/2 hidden md:block ">
+          <div className="absolute left-[36em] top-7 transform z-0 -translate-y-1/2 hidden md:block">
             <Form onFinish={handleSearch} layout="inline">
               <Form.Item
                 name="search"
-                rules={[
-                  { required: true, message: "Please enter your search text" },
-                ]}
+                rules={[{ required: true, message: "Please enter your search text" }]}
               >
                 <div className="relative">
                   <CiSearch
@@ -169,18 +163,6 @@ const Header: React.FC = () => {
                   />
                 }
               />
-              {/* {isAvatarHovered && (
-                <ul className="absolute top-[30px] right-0 bg-white border border-gray-300 dark:bg-gray-700 dark:border-gray-600">
-                  {profileMenuItems.map((item) => (
-                    <li
-                      key={item.key}
-                      className="py-2 px-4 text-gray-700 hover:bg-gray-100 cursor-pointer dark:text-white dark:hover:bg-gray-600"
-                    >
-                      {item.label}
-                    </li>
-                  ))}
-                </ul>
-              )} */}
             </div>
           </div>
 
@@ -189,38 +171,13 @@ const Header: React.FC = () => {
             <Dropdown.Button
               icon={<DownOutlined />}
               menu={{ items: buttonMenuItems }}
-              className="bg-[rgba(221,49,49,0.36)] hover:bg-[rgba(221,49,49,0.2)] text-white "
+              className="bg-[rgba(221,49,49,0.36)] hover:bg-[rgba(221,49,49,0.2)] text-white"
             >
               Add Question
             </Dropdown.Button>
-
-            {/* <div className="h-8 w-[9.5em] bg-red-500 rounded-[50px] align-middle border-1 border-black flex items-center justify-center relative">
-              <button className="text-white text-[15px] -tracking-tighter font-mono flex items-center">
-                Add Question
-              </button>
-              <div
-                onMouseEnter={() => setIsButtonHovered(true)}
-                onMouseLeave={() => setIsButtonHovered(false)}
-              >
-                <DownOutlined className="mx-1 cursor-pointer text-white hover:bg-red-400 p-1 rounded-lg" />
-
-                {isButtonHovered && (
-                  <ul className="absolute top-[29px] right-0 w-[8.5vw] bg-white border border-gray-300 dark:bg-gray-700 dark:border-gray-600">
-                    {buttonMenuItems.map((item) => (
-                      <li
-                        key={item.key}
-                        className="py-1 px-3 text-gray-700 hover:bg-gray-100 cursor-pointer dark:text-white dark:hover:bg-gray-600"
-                      >
-                        {item.label}
-                      </li>
-                    ))}
-                  </ul>
-                )}
-              </div>
-            </div> */}
           </div>
 
-          {/* Languge select icon */}
+          {/* Language select icon */}
           <div className="absolute top-3 right-[12em]">
             <div
               onMouseEnter={() => setIsButtonTranslate(true)}
@@ -230,18 +187,6 @@ const Header: React.FC = () => {
                 size={30}
                 className="text-gray-500 cursor-pointer hover:text-red-600 dark:text-white"
               />
-              {/* {isButtonTranslate && (
-                <ul className="absolute top-[30px] right-0 w-auto bg-white border border-gray-300 dark:bg-gray-700 dark:border-gray-600">
-                  {translateMenuItems.map((item) => (
-                    <li
-                      key={item.key}
-                      className="py-1 px-3 text-gray-700 hover:bg-gray-100 cursor-pointer dark:text-white dark:hover:bg-gray-600"
-                    >
-                      {item.label}
-                    </li>
-                  ))}
-                </ul>
-              )} */}
             </div>
           </div>
 
@@ -255,64 +200,71 @@ const Header: React.FC = () => {
         </div>
       </nav>
 
-      {/* Search Bar Responsive */}
+      {/* Navbar Responsive */}
       <div className="md:hidden">
-        <div className=" h-[6vh] bg-red-600">
+        <div className="h-[5.5vh] bg-red-600">
           <div className="flex text-white gap-1 absolute top-2">
-        <CiSearch
-          size={25}
-        />
-        <h2 className="text-[15px]">Search</h2>
-        </div>
-
-        <h2 className="text-white text-center text-[20px] py-1 font-mono">Query Hub</h2>
-
-                <div className="text-white absolute top-2 right-2 flex gap-1">
-        <PlusCircleOutlined  
-        size={25}/>
-                <h2 className="text-[15px]">Add</h2>
-
-                </div>
-
-
-
-
-
-
-
-
-        {/* <div className="">
-            <IoIosAddCircleOutline />
+            <CiSearch size={25} />
+            <h2 className="text-[15px]">Search</h2>
           </div>
 
-          <ul className="">
-            <li>
-              <MdHome
-                size={33}
-                className="text-gray-600 cursor-pointer hover:text-red-600 dark:text-white dark:hover:text-red-600"
-              />
-            </li>
-            <li>
-              <CiViewList
-                size={33}
-                className="text-gray-600 cursor-pointer hover:text-red-600 dark:text-white dark:hover:text-red-600"
-              />
-            </li>
-            <li>
-              <FaRegPenToSquare
-                size={30}
-                className="text-gray-500 cursor-pointer hover:text-red-600 dark:text-white dark:hover:text-red-600"
-              />
-            </li>
-            <li>
-              <IoMdNotificationsOutline
-                size={33}
-                className="text-gray-600 cursor-pointer hover:text-red-600 dark:text-white dark:hover:text-red-600"
-              />
-            </li>
-          </ul> */}
+          <h2 className="text-white text-center text-[20px] py-1 font-mono">
+            QueryHub
+          </h2>
 
-</div>
+          <div className="text-white absolute top-2 right-2 flex gap-1">
+            <PlusCircleOutlined size={25} />
+            <h2 className="text-[15px]">Add</h2>
+          </div>
+
+          <div className="h-[6.5vh] bg-gray-100">
+            <ul className="flex gap-[0.1em]">
+              <li className="border-2 border-gray-300 p-1 px-3">
+                <MdHome
+                  size={33}
+                  className="text-gray-600 cursor-pointer hover:text-red-600 dark:text-white dark:hover:text-red-600"
+                />
+              </li>
+              <li className="border-2 border-gray-300 p-1 px-3">
+                <CiViewList
+                  size={33}
+                  className="text-gray-600 cursor-pointer hover:text-red-600 dark:text-white dark:hover:text-red-600"
+                />
+              </li>
+              <li className="border-2 border-gray-300 p-1 px-3">
+                <FaRegPenToSquare
+                  size={33}
+                  className="text-gray-500 cursor-pointer hover:text-red-600 dark:text-white dark:hover:text-red-600"
+                />
+              </li>
+              <li className="border-2 border-gray-300 p-1 px-3">
+                <IoMdNotificationsOutline
+                  size={33}
+                  className="text-gray-600 cursor-pointer hover:text-red-600 dark:text-white dark:hover:text-red-600"
+                />
+              </li>
+              <li className="border-2 border-gray-300 p-1 px-3">
+                <Avatar
+                  icon={
+                    <Image
+                      src={img}
+                      alt="avatar"
+                      width={40}
+                      height={40}
+                      className="rounded-full cursor-pointer"
+                    />
+                  }
+                />
+              </li>
+              <li className="border-2 border-gray-300 p-1 px-3">
+                <MdLanguage
+                  size={30}
+                  className="text-gray-500 cursor-pointer hover:text-red-600 dark:text-white"
+                />
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
     </>
   );
